cc7ad63a184ae1d29136004937e975022bbd62d4
SonMyeongJin <smj2802@naver.com>
2025-10-19 21:41:58 +0900

    [Feat:] Create project with SpringBoot

---
+++ src/main/java/dbak/server/DBakSvApplication.java
@@ -0,0 +1,13 @@
+package dbak.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBakSvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DBakSvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dbak/server/DBakSvApplicationTests.java
@@ -0,0 +1,13 @@
+package dbak.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DBakSvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
